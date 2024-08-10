50ab5e97aa92822f48d7c6f78f7a75374ac78264
bibhashdash <b.dash@12os.co.uk>
2024-08-10 14:11:22 +0100

    functionality working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
-export interface GameCard {
+export interface BasicCard {
   imageSrc: string,
-  id: number,
+  matchableId: string,
 }
-const shuffle = (array: Array<GameCard>) => {
+export interface GameCard {
+  card: BasicCard,
+  id: string,
+}
+const shuffle = (array: Array<BasicCard>) => {
   const shuffledArray = [...array];
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -17,16 +21,27 @@ const getRandomNumber = () => {
   return Math.floor(Math.random() * 200);
 }
 
-export default function Home() {
+export interface DrawFlip {
+  id: string,
+  matchableId: string,
+}
 
-  const [cards, setCards] = useState<Array<GameCard>>([]);
+export default function Home() {
+  const [cards, setCards] = useState<Array<BasicCard>>([]);
   const [matchedCards, setMatchedCards] = useState<Array<GameCard>>([]);
+  const [firstFlip, setFirstFlip] = useState<DrawFlip | null>(null);
+  const [secondFlip, setSecondFlip] = useState<DrawFlip | null>(null);
+  const [numberOfClicks, setNumberOfClicks] = useState<number>(0);
   const resetGame = () => {
-    const temp: Array<GameCard> = [];
-    for (let i=0; i<8; i++) {
+    setFirstFlip(null);
+    setSecondFlip(null);
+    setNumberOfClicks(0);
+    const temp: Array<BasicCard> = [];
+    for (let i= 0; i < 8; i++) {
+      const imageTemp = `https://picsum.photos/id/${getRandomNumber()}/200`
       temp.push({
-        id: i,
-        imageSrc: `https://picsum.photos/id/${getRandomNumber()}/200`
+        imageSrc: imageTemp,
+        matchableId: `photo${i}`,
       })
     }
     setCards(temp);
@@ -38,38 +53,96 @@ export default function Home() {
 
   useEffect(() => {
     const temp2 = shuffle(cards);
-    setMatchedCards(temp2);
-  }, [cards])
+    const temp3 = [...cards, ...temp2]
+    const temp4: Array<GameCard> = temp3.map((item, index) => (
+      {
+        card: item,
+        id: `photo${index}`,
+      }
+    ))
+    console.log(temp4);
+    setMatchedCards(temp4)
+  }, [cards]);
+
+  const updateGameState = (id: string, matchableId: string) => {
+    if (firstFlip) {
+      setSecondFlip({
+        id: id,
+        matchableId: matchableId,
+      });
+    } else {
+      setFirstFlip({
+        id: id,
+        matchableId: matchableId,
+      });
+    }
+  }
+
+  useEffect(() => {
+    if (numberOfClicks > 0 && numberOfClicks % 2 === 0) {
+      if (firstFlip && secondFlip && firstFlip.matchableId === secondFlip.matchableId) {
+        cardsMatchedFeedback();
+      }
+      else {
+        rotateCardsBack();
+      }
+    }
+  }, [numberOfClicks])
+
+  const handleClick = (id: string, matchableId: string) => {
+    setNumberOfClicks(prevState => prevState + 1);
+    const element = document.querySelector(`#${id}`)
+    if (element) {
+      element.classList.add('rotate');
+      updateGameState(id, matchableId);
+    }
+  }
+
+  const rotateCardsBack = () => {
+    setTimeout(() => {
+      const card1 = document.querySelector(`#${firstFlip?.id}`)
+      const card2 = document.querySelector(`#${secondFlip?.id}`)
+      if (card1 && card2) {
+        card1.classList.remove('rotate');
+        card2.classList.remove('rotate');
+      }
+      setFirstFlip(null);
+      setSecondFlip(null);
+    }, 1000)
+  }
+
+  const cardsMatchedFeedback = () => {
+    const card1 = document.querySelector(`#${firstFlip?.id}`)
+    const card2 = document.querySelector(`#${secondFlip?.id}`)
+    if (card1 && card2) {
+      card1.classList.add('matched');
+      card2.classList.add('matched');
+      setTimeout(() => {
+        card1.classList.add('hidden')
+        card2.classList.add('hidden')
+      }, 500)
+    }
+    setFirstFlip(null);
+    setSecondFlip(null);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center gap-4">
       <div>Memory Snap Game</div>
       <button className="bg-gray-700 text-gray-50 px-2 py-1 rounded-md" onClick={resetGame}>Reset Game</button>
       <div className="grid grid-cols-4 gap-2">
         {
-          cards.map(item => (
-            <div key={item.id} className="flip-box">
-              <div className="flip-box-inner">
-                <div className="flex flex-col justify-center flip-box-front">
-                  <p className="text-xl font-extrabold text-blue-700">Memory Snap</p>
-                </div>
-                <div className="flip-box-back">
-                  <img src={item.imageSrc} alt="card-image" width="200" height="200" />
-                </div>
-              </div>
-            </div>
-          ))
-        }
-      </div>
-      <div className="grid grid-cols-4 gap-2">
-        {
           matchedCards.map(item => (
-            <div key={item.id} className="flip-box">
-              <div className="flip-box-inner">
+            <div
+              onClick={() => handleClick(item.id, item.card.matchableId)}
+              key={item.id} className="flip-box cursor-pointer"
+            >
+              <div id={item.id} className="flip-box-inner">
                 <div className="flex flex-col justify-center flip-box-front">
                   <p className="text-xl font-extrabold text-blue-700">Memory Snap</p>
                 </div>
                 <div className="flip-box-back">
-                  <img src={item.imageSrc} alt="card-image" width="200" height="200" />
+                  <img src={item.card.imageSrc} alt="card-image" width="200" height="200" />
                 </div>
               </div>
             </div>
